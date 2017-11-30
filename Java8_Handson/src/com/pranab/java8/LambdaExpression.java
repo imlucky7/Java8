08b98bac49024b9172200c349632b8c34e93a538
Pranab Dutta <v-pranab.d@turvo.com>
2017-11-30 16:57:41 +0530

    Lambda expression example

---
+++ Java8_Handson/src/com/pranab/java8/LambdaExpression.java
@@ -0,0 +1,119 @@
+package com.pranab.java8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.Stream;
+
+class LambdaExpression {
+
+	public static void main(String[] args) {
+		/*
+		 * 1. Simple example of lambda expression
+		 */
+		Task task = name -> 	System.out.println("Task assigned for " + name);
+		task.assignTask("Pranab");
+		
+		/*
+		 * 2 .Foreach Loop lambda expression
+		 */
+		String[] name = {"Helen", "Joie", "Tom" };
+		List<String> list = Arrays.asList(name);
+		
+		list.forEach(
+				//nm -> System.out.println(nm)
+				System.out::println
+		);
+		
+		/*
+		 * 3. Thread using lambda expression
+		 */
+		new Thread(
+				() -> System.out.println("Thread is running...")
+		).start();
+		
+		/*
+		 * 4. Comparator using lambda expression
+		 */
+		
+		Student[] students = {new Student("Tamal", "Seven", 540), new Student("Rakesh", "Seven", 490), new Student("Emon", "Seven", 543)};
+		List<Student> studentList = Arrays.asList(students);
+		
+		studentList.forEach(std -> System.out.println(".."+std));
+		
+		Collections.sort(studentList, (s1, s2) -> {
+			return s1.getName().compareTo(s2.getName());
+		});
+		
+		studentList.forEach(std -> System.out.println("......"+std));
+		
+		/*
+		 * 5. Filter collection using lambda expression
+		 */
+		Stream<Student> stream = studentList.stream().filter(s -> s.getMarks()>500);
+		stream.forEach(s -> System.out.println(".........."+s));
+		
+		/*
+		 * 6. Passing lambda expression in method argument
+		 * java.util.function package enlists a lot of functional interface 
+		 * for lambda expression use
+		 */
+		Student s = new Student("Elsa", "Six", 334);
+		s.greeting(400, (passMark) -> {
+			return s.getMarks() > passMark ? "Congratulations "+s.getName()+" !! you are promoted" : "Sorry "+s.getName()+" !! you are not promoted";
+		});
+	}
+
+}
+
+ @FunctionalInterface
+interface Task {
+	public void assignTask(String name);
+}
+
+class Student {
+	private String name;
+	private String std;
+	private int marks;
+	
+	public Student(String name, String std, int marks) {
+		this.name = name;
+		this.std = std;
+		this.marks = marks;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStd() {
+		return std;
+	}
+
+	public void setStd(String std) {
+		this.std = std;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", std=" + std + ", marks=" + marks + "]";
+	}
+	
+	public void greeting(int marks, IntFunction<String> fn) {
+		System.out.println(fn.apply(marks));
+	}
+}
